32fa501f24c62ed074d341f33f0f5c67d95a6bb0
serhi2010 <147245469+serhi2010@users.noreply.github.com>
2025-02-13 21:17:43 +0200

    def_run

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import pYgame
+import pygame
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption("Battle_City")
+    bg_color = (0, 0, 0)
+    
+run()    
