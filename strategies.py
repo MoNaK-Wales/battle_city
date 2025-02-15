6557c4e15009cb287a3050a429e4b6d36ad2c027
Maksim <60656307+MoNaK-Wales@users.noreply.github.com>
2025-02-15 18:19:20 +0200

    Scenes
    * full Menu, Stage started, transition of main to SceneBase
    
    * gitignore init
    
    * hero class changes + sprites

--- strategies.py
+++ strategies.py
@@ -1,13 +1,12 @@
 import pygame 
 import pygame.math
-from battle_city.settings import *
+from constants import *
 from abc import ABC, abstractmethod
 
 
 
 class Move_Strategy(ABC):
-    def __init__(self, move, entity):
-        self.move = move
+    def __init__(self, entity):
         self.entity = entity
     
     @abstractmethod
